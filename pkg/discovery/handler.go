cc36a1b487b44f78b62a80dbf63bec79954260d7
Nacho Cano <nachoacano@gmail.com>
2020-08-15 00:58:24 -0700

    updates to handler

--- pkg/discovery/handler.go
+++ pkg/discovery/handler.go
@@ -18,10 +18,13 @@ package discovery
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-	"net/url"
 	"strings"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"k8s.io/apimachinery/pkg/labels"
 
 	"go.uber.org/zap"
 	eventingv1 "knative.dev/eventing/pkg/apis/eventing/v1"
@@ -48,7 +51,7 @@ type Handler struct {
 	Logger *zap.Logger
 }
 
-func (h *Handler) getBroker(name, namespace string) (*eventingv1.Broker, error) {
+func (h *Handler) getBroker(namespace, name string) (*eventingv1.Broker, error) {
 	broker, err := h.BrokerLister.Brokers(namespace).Get(name)
 	if err != nil {
 		h.Logger.Warn("Broker getter failed")
@@ -57,22 +60,72 @@ func (h *Handler) getBroker(name, namespace string) (*eventingv1.Broker, error) 
 	return broker, nil
 }
 
-func (h *Handler) getEventType(name, namespace string) (*eventingv1beta1.EventType, error) {
-	eventType, err := h.EventTypeLister.EventTypes(namespace).Get(name)
+func (h *Handler) getBrokerById(namespace, id string) (*eventingv1.Broker, error) {
+	brokers, err := h.BrokerLister.Brokers(namespace).List(labels.Everything())
 	if err != nil {
-		h.Logger.Warn("EventType getter failed")
+		h.Logger.Warn("Brokers list failed")
 		return nil, err
 	}
-	return eventType, nil
+	for _, broker := range brokers {
+		if string(broker.UID) == id {
+			return broker, nil
+		}
+	}
+	h.Logger.Warn("Broker not found", zap.String("id", id))
+	return nil, errors.NewNotFound(schema.GroupResource{}, "")
 }
 
-func guessChannelAddress(name, namespace, domain string) string {
-	url := url.URL{
-		Scheme: "http",
-		Host:   fmt.Sprintf("%s-kne-trigger-kn-channel.%s.svc.%s", name, namespace, domain),
-		Path:   "/",
+func (h *Handler) getBrokers(namespace string) ([]*eventingv1.Broker, error) {
+	brokers, err := h.BrokerLister.Brokers(namespace).List(labels.Everything())
+	if err != nil {
+		h.Logger.Warn("Brokers list failed")
+		return nil, err
 	}
-	return url.String()
+	return brokers, nil
+}
+
+// TODO this could actually return a list. Correct spec.
+func (h *Handler) getEventType(namespace, type_ string) (*eventingv1beta1.EventType, error) {
+	eventTypes, err := h.EventTypeLister.EventTypes(namespace).List(labels.Everything())
+	if err != nil {
+		h.Logger.Warn("EventTypes list failed")
+		return nil, err
+	}
+	for _, eventType := range eventTypes {
+		if eventType.Spec.Type == type_ {
+			return eventType, nil
+		}
+	}
+	h.Logger.Warn("EventType not found", zap.String("type", type_))
+	return nil, errors.NewNotFound(schema.GroupResource{}, "")
+}
+
+func (h *Handler) getEventTypes(namespace string) ([]*eventingv1beta1.EventType, error) {
+	eventTypes, err := h.EventTypeLister.EventTypes(namespace).List(labels.Everything())
+	if err != nil {
+		h.Logger.Warn("EventTypes list failed")
+		return nil, err
+	}
+	return eventTypes, nil
+}
+
+func (h *Handler) getEventTypesMatching(namespace, typeSubstring string) ([]*eventingv1beta1.EventType, error) {
+	eventTypes, err := h.EventTypeLister.EventTypes(namespace).List(labels.Everything())
+	if err != nil {
+		h.Logger.Warn("EventTypes list failed")
+		return nil, err
+	}
+	ets := make([]*eventingv1beta1.EventType, 0)
+	for _, eventType := range eventTypes {
+		if strings.Contains(strings.ToLower(eventType.Spec.Type), strings.ToLower(typeSubstring)) {
+			ets = append(ets, eventType)
+		}
+	}
+	if len(ets) == 0 {
+		h.Logger.Warn("EventTypes not found", zap.String("typeSubstring", typeSubstring))
+		return nil, errors.NewNotFound(schema.GroupResource{}, "")
+	}
+	return ets, nil
 }
 
 func (h *Handler) Start(ctx context.Context) error {
@@ -94,13 +147,130 @@ func (h *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusNotFound)
 		return
 	}
-	nsBrokerName := strings.Split(request.RequestURI, "/")
-	if len(nsBrokerName) != 3 {
+
+	requestURI := strings.Split(request.RequestURI, "/")
+
+	for i, req := range requestURI {
+		h.Logger.Info("URI", zap.Int("idx", i), zap.String("req", req))
+	}
+
+	if len(requestURI) != 4 && len(requestURI) != 5 {
 		h.Logger.Info("Malformed uri", zap.String("URI", request.RequestURI))
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	// ctx := request.Context()
-	writer.WriteHeader(500)
+	// Only allow namespaced-based queries for now.
+	if requestURI[1] != "namespaces" {
+		h.Logger.Info("Malformed uri", zap.String("URI", request.RequestURI))
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	namespace := requestURI[2]
+
+	if requestURI[3] == "services" {
+		h.handleServices(writer, request, requestURI, namespace)
+	} else if requestURI[3] == "types" {
+		h.handleTypes(writer, request, requestURI, namespace)
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (h *Handler) handleServices(writer http.ResponseWriter, request *http.Request, requestURI []string, namespace string) {
+	if len(requestURI) == 5 {
+		// /namespaces/<namespace>/services/{id}
+		broker, err := h.getBrokerById(namespace, requestURI[4])
+		if err != nil {
+			if errors.IsNotFound(err) {
+				writer.WriteHeader(http.StatusNotFound)
+				return
+			}
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		h.Logger.Info("Broker retrieved", zap.String("broker", broker.Name))
+	} else {
+		req := strings.Split(requestURI[3], "?")
+		if len(req) > 2 {
+			h.Logger.Info("Malformed uri", zap.String("URI", request.RequestURI))
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		} else if len(req) == 2 {
+			//  /namespaces/<namespace>/services?name={name}
+			r := strings.Split(req[2], "=")
+			if len(r) != 2 || r[0] != "name" {
+				h.Logger.Info("Malformed uri", zap.String("URI", request.RequestURI))
+				writer.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			broker, err := h.getBroker(namespace, r[1])
+			if err != nil {
+				if errors.IsNotFound(err) {
+					writer.WriteHeader(http.StatusNotFound)
+					return
+				}
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			h.Logger.Info("Broker retrieved", zap.String("broker", broker.Name))
+		} else {
+			// /namespaces/<namespace>/services
+			brokers, err := h.getBrokers(namespace)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			h.Logger.Info("Brokers retrieved", zap.Int("brokersCount", len(brokers)))
+		}
+	}
+}
+
+func (h *Handler) handleTypes(writer http.ResponseWriter, request *http.Request, requestURI []string, namespace string) {
+	if len(requestURI) == 5 {
+		// /namespaces/<namespace>/types/{type}
+		et, err := h.getEventType(namespace, requestURI[4])
+		if err != nil {
+			if errors.IsNotFound(err) {
+				writer.WriteHeader(http.StatusNotFound)
+				return
+			}
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		h.Logger.Info("EventType retrieved", zap.String("eventType", et.Name))
+	} else {
+		req := strings.Split(requestURI[3], "?")
+		if len(req) > 2 {
+			h.Logger.Info("Malformed uri", zap.String("URI", request.RequestURI))
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		} else if len(req) == 2 {
+			// /namespaces/<namespace>/types?matching={name}
+			r := strings.Split(req[2], "=")
+			if len(r) != 2 || r[0] != "matching" {
+				h.Logger.Info("Malformed uri", zap.String("URI", request.RequestURI))
+				writer.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			ets, err := h.getEventTypesMatching(namespace, r[1])
+			if err != nil {
+				if errors.IsNotFound(err) {
+					writer.WriteHeader(http.StatusNotFound)
+					return
+				}
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			h.Logger.Info("EventTypes retrieved", zap.Int("eventTypesCount", len(ets)))
+		} else {
+			// /namespaces/<namespace>/types
+			ets, err := h.getEventTypes(namespace)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			h.Logger.Info("EventTypes retrieved", zap.Int("eventTypesCount", len(ets)))
+		}
+	}
 }
